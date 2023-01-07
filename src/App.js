64eaec70aa547a721bf0470728a16f4e5cb027d8
Mukul Kolpe <mukulkolpe45@gmail.com>
2023-01-07 06:13:20 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  LivepeerProvider,
+  useCreateAsset,
+  useLivepeerProvider,
+} from "@livepeer/react";
+import React, { useState } from "react";
+import { Player } from "@livepeer/react";
+
+import blenderPoster from "./assets/thumbnail.jpg";
 
 function App() {
+  const provider = useLivepeerProvider();
+  const [video, setVideo] = useState(null);
+
+  const {
+    mutate: createAsset,
+    data: assets,
+    status,
+    progress,
+    error,
+  } = useCreateAsset(
+    video
+      ? {
+          sources: [{ name: video.name, file: video }],
+        }
+      : null
+  );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Hello, {provider.getConfig().name}</h2>
+      <input
+        type={"file"}
+        onChange={(e) => {
+          setVideo(e.target.files[0]);
+        }}
+      />
+      <button
+        disabled={status === "loading" || !createAsset}
+        onClick={() => {
+          createAsset?.();
+        }}
+      >
+        Create Asset
+      </button>
+      {assets?.map((asset) => (
+        <div key={asset.id}>
+          <div>
+            <div>Asset Name: {asset?.name} </div>
+            <div>Playback URL: {asset?.playbackUrl}</div>
+            <div>IPFS CID: {asset?.storage?.ipfs?.cid ?? "None"}</div>
+            <div>Player Back ID: {asset?.playbackId}</div>
+          </div>
+        </div>
+      ))}
+
+      {error && <div>{error.message}</div>}
+      <div className="video-player">
+        {assets ? (
+          <Player
+            title={assets[0]?.name}
+            playbackId={assets[0]?.playbackId}
+            loop
+            poster={
+              <img
+                className="video"
+                src={blenderPoster}
+                layout="fill"
+                objectFit="cover"
+                priority
+                placeholder="blur"
+                alt="Random Thumbnail"
+              />
+            }
+            showPipButton
+            objectFit="cover"
+          />
+        ) : null}
+        {/* <Player
+          title="localhost_hack-on-chain_submission"
+          playbackId={assets[0]?.playbackId}
+          loop
+          poster={
+            <img
+              className="video"
+              src={blenderPoster}
+              layout="fill"
+              objectFit="cover"
+              priority
+              placeholder="blur"
+              alt="Random Thumbnail"
+            />
+          }
+          showPipButton
+          objectFit="cover"
+        /> */}
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  createReactClient,
+  studioProvider,
+  LivepeerConfig,
+} from "@livepeer/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const client = createReactClient({
+  provider: studioProvider({
+    apiKey: process.env.REACT_APP_LIVEPEER_API_KEY,
+  }),
+  config: LivepeerConfig.studio,
+});
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <LivepeerConfig client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </LivepeerConfig>
 );
 
 // If you want to start measuring performance in your app, pass a function
